bd89e2d0eb6ca152e138012bd782466a680d9a32
THEETHAWAT SAVASTHAM <5910110150@email.psu.ac.th>
2018-10-03 21:26:19 +0700

    Checkpoint 4

--- components/Weather.js
+++ components/Weather.js
@@ -11,7 +11,25 @@ export default class Weather extends React.Component {
           }
       }
   }
-  //fetchData = ()
+  fetchData = () =>{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.props.zipCode},th&units=metric&APPID=fd68c0f2039c5a25f666a9ff374bc93e`)
+      .then((response)=>response.json())
+      .then((json)=>{
+        this.setState(
+          {
+            forecast :{
+              main:json.weather[0].main,
+              description:json.weather[0].description,
+              temp:json.weather[0].temp
+            }
+          }
+        );
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }
+  componentDidMount = () =>this.fetchData()
   render() {
     return (
       <View style={styles.container}>
